3fdc9b8485b707aeb20261c605b4e2dda2f100c2
Nicholas Clark <nick@ccl4.org>
2013-03-02 12:07:33 +0100

    Add dist/constant/lib to the paths in the generated lib/buildcustomize.pl
    Strictly, add it to write_buildcustomize.pl, and so that the absolute path
    is added to the generated file.

--- write_buildcustomize.pl
+++ write_buildcustomize.pl
@@ -28,6 +28,7 @@ my @toolchain = qw(cpan/AutoLoader/lib
 		   cpan/File-Path/lib
 		   ext/re
 		   dist/Term-ReadLine/lib
+		   dist/constant/lib
 		   );
 
 # Used only in ExtUtils::Liblist::Kid::_win32_ext()
